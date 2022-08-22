de0e99ad9e07c2f1a954439a36f248bf363db7a2
ezg <emanuel.zgraggen@gmail.com>
2022-08-22 11:48:58 -0400

    init

--- ezpackage/__init__.py
+++ ezpackage/__init__.py
@@ -4,6 +4,8 @@ pyexample.
 An example python library.
 """
 
+from ezpackage import funnel
+
 __version__ = "0.1.0"
-__author__ = 'Emanuel Zgraggen 1'
+__author__ = 'Emanuel Zgraggen 2'
 __credits__ = ''
